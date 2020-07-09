d9f1030cd4cc3b52dfb8fe2ce185f2d0996e262f
Ouzy012 <ousmane.ndiaye8518@gmail.com>
2020-07-09 21:19:14 +0000

    receive data on backend

---
+++ src/Config.js
@@ -0,0 +1,4 @@
+const BASE_URL = 'http://127.0.0.1:8000';
+const BASE_IMG = 'http://127.0.0.1:8000/image_auto/';
+
+export { BASE_URL, BASE_IMG };
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -2,30 +2,53 @@ import React, { Component } from 'react';
 import './Dashboard.css';
 import Voiture from '../../components/voiture/Voiture';
 import Header from '../../components/header/Header';
+import axios from 'axios';
+import { BASE_URL } from '../../../src/Config';
 
 class Dashboard extends Component {
 
     constructor() {
         super();
         this.state = {
-            url: ''
+            url: '',
+            automobiles: [],
+            loading: false,
+            nbre_pages: 0,
+            nbre_auto: 0
         }
     }
 
     componentDidMount(){
         this.gerUrl();
+        //Chargement de la page avant arrivee des donnees dans la base
+        this.setState({ loading: true });
+        //Requete base de données
+        axios.get(`${BASE_URL}/api/automobiles`)
+        .then(
+            response => {
+                const { data } = response;
+
+                if(data.status === 'success') {
+                    let automobiles = data.automobiles;
+                    if (data.nbre_auto > 6 ) {
+                        this.setState({ nbre_pages : data.nbre_auto / 6 });
+                    }
+                    this.setState({ automobiles, loading: false, nbre_auto: data.nbre_auto });
+                }
+            },
+
+            error => {
+                console.log('erreur', error);
+            }
+        );
     }
 
     gerUrl = () =>{
-        //this.setState({ url: window.location.pathname })
-        //this.setState({ url: this.props.location.pathname })
         this.setState({ url: window.location.href})
     }
 
     render() {
-        const { url } = this.state;
-        console.log('url', url);
-        
+        const { url, automobiles, loading, nbre_pages, nbre_auto } = this.state;
 
         return (
             <>
@@ -35,22 +58,42 @@ class Dashboard extends Component {
                         <div className="card-body">
                             <div className="row mt-2">
 
-                                <Voiture />
-                                <Voiture />
-                                <Voiture />
-                                <Voiture />
-                                <Voiture />
-                                <Voiture />
+                                {
+                                    loading ? 
+                                    <div className="d-flex justify-content-center text-center">
+                                        <div className="spinner-border spinner-color" role="status">
+                                            <span className="sr-only">Loading...</span>
+                                        </div>
+                                    </div> :
+                                    automobiles.map((auto, key) => 
+                                        <Voiture value={ auto } />
+                                    )
+                                }
 
                             </div>
                         </div>
-                        <span className="text-center">
-                            <a href="#" className="glyphicon glyphicon-backward">Page Précédente</a>
-                            <span>...</span>
-                            <a href="#">
-                                <span className="glyphicon glyphicon-chevron-right">Page Suivante</span>
-                            </a>
-                        </span>
+                        {
+                            nbre_auto <= 6 ? '' :
+                                <div className="card-footer py-2">
+                                    <nav aria-label="...">
+                                        <ul className="pagination justify-content-end">
+                                            <li className="page-item">
+                                                <a className="page-link" href="#" aria-label="Previous">
+                                                    <span aria-hidden="true">&laquo;</span>
+                                                </a>
+                                            </li>
+                                            <li className="page-item"><a className="page-link" href="#">1</a></li>
+                                            <li className="page-item"><a className="page-link"> ... </a></li>
+                                            <li className="page-item"><a className="page-link" href="#">{ nbre_pages > 1 ? Math.floor(nbre_pages + 1) : '' }</a></li>
+                                            <li className="page-item">
+                                                <a className="page-link" href="#" aria-label="Next">
+                                                    <span aria-hidden="true">&raquo;</span>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                        }
                     </div>
                 </div>
             </>
